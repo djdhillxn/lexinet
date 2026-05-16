7f8c49e8ffc4d5b2d759e1a94c2e3d7fe89c39dd
djdhillxn <dhillondheeraj84@gmail.com>
2026-05-16 17:50:43 -0500

    removing redundancies.. without losing any functionality

--- src/train.py
+++ src/train.py
@@ -40,35 +40,6 @@ class NgramModel:
         with open(path, 'wb') as f:
             pickle.dump(data, f)
 
-    def train_gold(self, data):
-        for word in tqdm(data):
-            padded_word = ['<s>'] * (self.n - 1) + list(word) + ['</s>'] * (self.n - 1)
-            for gram in ngrams(padded_word, self.n):
-                prefix = gram[:-1]
-                suffix = gram[-1]
-                if suffix == '</s>':
-                    continue
-                self.ngrams[prefix][suffix] += 1
-                if self.n > 2:
-                    potential_mask_indices = [0]*(self.n-1)
-                    cnt = 0
-                    for i in range(self.n - 1):
-                        if gram[i] not in ['<s>', '</s>']:
-                            potential_mask_indices[i] = 1
-                            cnt += 1
-                        
-                    num_masks = max(cnt-1, self.n - 2)
-                    if num_masks > 0:
-                        for i in range(1, num_masks+1):
-                            for mask_indices in combinations([j for j, x in enumerate(potential_mask_indices) if x == 1], i):
-                                masked_gram = list(gram[:-1])
-                                for index in mask_indices:
-                                    masked_gram[index] = '_'
-                                masked_gram = tuple(masked_gram)
-                                prefix = masked_gram[:]
-                                suffix = gram[-1]
-                                self.ngrams[prefix][suffix] += 1
-
     def train(self, data):
         for word in tqdm(data):
             padded_word = ['<s>'] * (self.n - 1) + list(word) + ['</s>'] * (self.n - 1)
@@ -99,35 +70,6 @@ class NgramModel:
                                 suffix = gram[-1]
                                 self.ngrams[prefix][suffix] += 1
                                 self.continuation_counts[suffix][prefix] += 1
-
-    def train_reverse_gold(self, data):
-        for word in tqdm(data):
-            padded_word = ['<s>'] * (self.n - 1) + list(word) + ['</s>'] * (self.n - 1)
-            for gram in ngrams(padded_word, self.n):
-                prefix = gram[0]
-                suffix = gram[1:]
-                if prefix == '<s>':
-                    continue
-                self.ngrams_rev[suffix][prefix] += 1
-                if self.n > 2:
-                    potential_mask_indices = [0] * (self.n - 1)
-                    cnt = 0
-                    for i in range(self.n-1):
-                        if gram[i+1] not in ['<s>', '</s>']:
-                            potential_mask_indices[i] = 1
-                            cnt += 1
-
-                    num_masks = max(cnt-1, self.n - 2)
-                    if num_masks > 0:
-                        for i in range(1, num_masks+1):
-                            for mask_indices in combinations([j for j, x in enumerate(potential_mask_indices) if x == 1], i):
-                                masked_gram = list(gram[1:])
-                                for index in mask_indices:
-                                    masked_gram[index] = '_'
-                                masked_gram = tuple(masked_gram)
-                                prefix = gram[0]
-                                suffix = masked_gram[:]
-                                self.ngrams_rev[suffix][prefix] += 1
 
     def train_reverse(self, data):
         for word in tqdm(data):
